679605f0b8a738cf6c6bd53fff3d27c88a06d080
marroccuandrea <marroccuandrea@yahoo.it>
2024-03-01 15:43:21 +0100

    creata funzione che genera 16 numeri casuali + array di contenimento

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ function init(){
     reset();
     for(let i = 1; i <= 100; i++){
         const square = getSquare(i);
+        let bombs = bombGenerator[i];
         grid.append(square);
     }
 }
@@ -19,11 +20,12 @@ function getSquare(indice){
     const square = document.createElement('div');
     square.className = 'square';
     // Metto in evidenza i numeri dei quadrati, se tolto vengono nascosti fino al click
-    square.innerHTML = indice;
+    // square.innerHTML = indice;
     square.sqID = indice;
     // Funzione che mi restituirà l'elemento che clicco
     square.addEventListener('click', function(){
-        this.innerHTML = this.sqID;
+        // Elemento che fa apparire visivamente i numeri delle celle che premo
+        // this.innerHTML = this.sqID;
         console.log(this.sqID);
         // Al click cambia colore, prendendo la classe creata in precedenza (clicked)
         this.classList.toggle('clicked');
@@ -31,6 +33,19 @@ function getSquare(indice){
 
     return square;
 }
+
+// Create a function that generates 16 random numbers
+
+function bombGenerator (){
+    let arrBombs = [];
+    for(let i = 0; i < 16; i++ ){
+        let numBomba  = Math.floor(Math.random() * 100) + 1;
+        arrBombs.push(numBomba);
+    }
+    return arrBombs;
+}
+
+
 
 // Funzione reset
 function reset(){
